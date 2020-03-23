12d0f880e635c2e8f09bcecb095071fdfdb1ca8a
Luis Osorio Lutzen <luis.lutzen@gmail.com>
2020-03-23 19:45:48 +0100

    main app and register app for django login microservice added.

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+
+def home(response):
+    return render(response,"main/home.html", {})
+
+
+def register(response):
+    return render(response, "main/register.html",{})
+
